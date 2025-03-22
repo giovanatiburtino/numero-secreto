95008a613d954205ec37c56f2b0c6b23b9dd82c1
giova <giovanaftiburtino@gmail.com>
2025-03-22 12:35:33 -0300

    projeto inicial

---
+++ app.js
@@ -0,0 +1,33 @@
+alert("Boas vindas ao número do jogo secreto");
+let numeroMaximo = 1000;
+let numeroSecreto = parseInt(Math.random() * numeroMaximo + 1)
+let chute;
+let tentativas = 1;
+
+
+while(chute != numeroSecreto){
+   chute = prompt(`Escolha um número entre 1 e ${numeroMaximo}`);
+
+    if(chute == numeroSecreto){
+        break;
+        
+    } else {
+        if(chute > numeroSecreto){
+            alert(`O número secreto é menor que ${chute}`);
+        } else {
+            alert(`O número secreto é maior que ${chute}`);
+        }
+
+        tentativas++;// tentativas = tentativas + 1
+    }
+
+}
+
+let palavraTentativa = tentativas > 1 ? "tentativas" : "tentativa"
+alert(`Você descobriu o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}!`);
+
+
+
+
+
+
